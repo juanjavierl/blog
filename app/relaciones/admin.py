cf429091b6f469f9e7fa9a2b8278a1ed11d548b7
jjavierl <jjavierl.bo@gmail.com>
2023-12-07 11:55:01 -0400

    migracione entre modelos

---
+++ app/relaciones/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Lugar)
+admin.site.register(Restaurant)
+
+
+admin.site.register(Reporter)
+admin.site.register(Article)
+
+
+admin.site.register(Publication)
+admin.site.register(Articulo)
---
+++ app/relaciones/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+# Create your models here.
+class Lugar(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"{self.name} el lugar"
+
+
+class Restaurant(models.Model):
+    lugar = models.OneToOneField(Lugar,on_delete=models.CASCADE,primary_key=True,)
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s the restaurant" % self.lugar.name
+
+
+class Mesero(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s El mesero en %s" % (self.name, self.restaurant)
+
+
+
+class Reporter(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
+
+    class Meta:
+        ordering = ["headline"]
+
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Articulo(models.Model):
+    headline = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Publication)
+
+    class Meta:
+        ordering = ["headline"]
+
+    def __str__(self):
+        return self.headline
